2f1b25f2912207165e32a8ed1c166fa07a08b95b
Nathan Lee <nathan.l.lee123@gmail.com>
2020-12-13 00:48:04 -0800

    UI overhaul

--- src/App.js
+++ src/App.js
@@ -1,125 +1,112 @@
-import React, {useState, useRef} from 'react'
+import React, {useState, useRef, useEffect} from 'react'
 
 import { createFFmpeg, fetchFile } from '@ffmpeg/ffmpeg';
 import resemble  from 'resemblejs'
 import './App.css';
 
-import Slide  from './slide'
+import LoadingBar from 'react-top-loading-bar'
 
+import Slide  from './slide'
+import LoadingSlide from './loadingSlide'
 
 function App() {
   const [videoSrc, setVideoSrc] = useState();
-  const [message, setMessage] = useState('Upload');
-  const [difference, setDifference] = useState();
-  const [diffCount, incCount] = useState(0);
   const [file, changeFile] = useState();
   const [imgSrc, setImg] = useState();
   const [imgSrc2, setImg2] = useState();
-
+  const [fileUploaded, setUpload] = useState(false);
   const [slides, addSlide] = useState([]);
 
-  const canvas = useRef(null);
+  const canvasRef = useRef(null);
+  const videoRef = useRef(null);
+
+  const [progress, setProgress] = useState(0)
+  const [startingData, changeStart] = useState("input")
 
   const ffmpeg = createFFmpeg({
     log: false,
   });
 
-  const onFileUpload = async () => {
-    setMessage('Loading ffmpeg-core.js');
-    await ffmpeg.load();
-    setMessage('Start transcoding');
 
+  var context;
+  useEffect(() => {
+    context = canvasRef.current.getContext('2d')
+    context.drawImage(videoRef.current, 0, 0, canvasRef.width, canvasRef.height);
+  })
+
+
+
+  const onFileUpload = async () => {
     if (file) {
-      ffmpeg.FS('writeFile', "vid.mp4", await fetchFile(file))
-      const data = ffmpeg.FS('readFile', 'vid.mp4');
-      await setVideoSrc(URL.createObjectURL(new Blob([data.buffer], { type: 'video/mp4' })));
+      changeStart("inputHidden")
+
+      setTimeout(() => {
+        changeStart("hidden")
+      }, 2000)
+
+      await setVideoSrc(URL.createObjectURL(new Blob([file], { type: 'video/mp4' })));
+      setUpload(true);
+
+
     }
+  }
+
+  const sleep = (milliseconds) => {
+    return new Promise(resolve => setTimeout(resolve, milliseconds))
   }
 
   const onVideoIn = async (length) => {
     await ffmpeg.load();
     ffmpeg.FS('writeFile', "vid.mp4", await fetchFile(file)) // Write mp4 file to memory
-    // Constants:
-    const frameSkip = 50;
     const differenceThresh = 4;
 
     var i = 0;
     while (i < length) {
-      var primaryImageList = []
       // get frame
       var time = secondsToTime(i)
-
-      const imgUrl = await getImageAtTime(time, "myimage.jpg");
-      //setImg(imgUrl);
-      primaryImageList.push(imgUrl);
-
+      const imgUrl = await getImageAtTime(i);
       var same = true;
-      var offset = i + frameSkip;
 
+      var j = i + 1;
       while (same) {
-        // get next frame
-        var nexttime = secondsToTime(offset)
-
-        const img2Url = await getImageAtTime(nexttime, "myimage2.jpg")
-        //setImg2(img2Url);
+        setProgress((j / length ) * 100);
+        var nexttime = secondsToTime(j)
+        const img2Url = await getImageAtTime(j)
 
         // if meets threshold, add split into document
-        if (await compareImages(imgUrl, img2Url, differenceThresh)) { // true is different, false is same
+        if (await compareImages(imgUrl, img2Url, differenceThresh) && (j - i > 30)) { // true is different, false is same // plus size should be mroe than 30 seconds
+          var vidUrl = await getVideo(i, j, "out.mp4")
+          addSlide(slides => slides.concat(<Slide vid={vidUrl} key={i} img={imgUrl} time={secondsToTime(i)}/>))
 
-          setDifference("Found new slide!")
-
-          var diffImage = true;
-          var imageList = [];
-          while (diffImage) { // Go backwards until the image changes (should be the start)
-            offset -= 5;
-            var revtime = secondsToTime(offset)
-            const revUrl = await getImageAtTime(revtime, "rev.jpg")
-            imageList.push(revUrl);
-            console.log(offset);
-            if (! (await compareImages(revUrl, imgUrl, differenceThresh))) { // If they're the same image
-              offset += 5
-
-              var vidUrl = await getVideo(i, offset, "out.mp4")
-              console.log(i, offset)
-
-              addSlide(slides => slides.concat(<Slide vid={vidUrl} key={i} img={imgUrl} time={secondsToTime(offset)}/>))
-              incCount(diffCount => diffCount + 1);
-              i = offset + 1
-
-              imageList.forEach(url =>{ URL.revokeObjectURL(url); console.log(url)});
-              primaryImageList.forEach(url =>{ URL.revokeObjectURL(url); console.log(url)});
-
-              same = false;
-              diffImage = false;
-            }
-          }
+          same = false;
+          i = j + 2;
         }
-        offset += frameSkip;
+        j += 1;
       }
-
-      //setMessage(`time: ${time}`)
-      i += frameSkip;
-      setMessage(i + " " + frameSkip)
+      i += 1;
     }
+
+    setProgress(0);
   }
 
   const secondsToTime = (sec) => {
     return new Date((sec) * 1000).toISOString().substr(11, 8)
   }
 
+
   const getImageAtTime = async(time, output) => {
-    await ffmpeg.run(...(`-ss ${time} -i vid.mp4 -vframes 2 -q:v 2 ${output}`).split(" "))
-    return getImUrl(output);
+    videoRef.current.currentTime = await time;
+
+    await context.drawImage(videoRef.current, 0, 0, canvasRef.current.width, canvasRef.current.height);
+
+    var blobURL = await canvasRef.current.toDataURL();
+    await sleep(50)
+    return blobURL;
   }
 
-  const getImUrl = async(image) => {
-    const img = await ffmpeg.FS('readFile', image)
-    return URL.createObjectURL(new Blob([img.buffer], { type: 'image/jpeg' }));
-  }
 
   var different;
   const compareImages = async (img1, img2, threshold) => {
-
     var diff = await resemble(img1)
       .compareTo(img2)
       .onComplete(data => {
@@ -145,34 +132,35 @@ function App() {
     changeFile(e.target.files[0]);
   }
 
+
+
   return (
     <div className="App">
-      <p/>
+      <div className="header">
+        <h1 className="name">Slides to Article</h1>
+      </div>
 
-      <video src={videoSrc} width="250" controls
+      <LoadingBar color="#f11946" progress={progress} onLoaderFinished={() => setProgress(0)}/>
+
+      <div className={startingData}>
+        <h1>Upload a file to get started</h1>
+        <input type="file" onChange={onFileChange} />
+        <button onClick={onFileUpload}>
+          Upload!
+        </button>
+      </div>
+
+      <video className="mainVid" ref={videoRef} src={videoSrc} width="250" controls
       onLoadedMetadata={e => {
         onVideoIn(e.target.duration);
-      }}></video><br/>
+      }}></video>
 
-      <canvas ref={canvas}></canvas>
-
-      <img width="250" src={imgSrc}></img>
-      <img width="250" src={imgSrc2}></img>
-
-
-      <p>Measured Difference: {difference}</p>
-      <p>Number of slides: {diffCount}</p>
-      <p>{message}</p>
-
-      <input type="file" onChange={onFileChange} />
-      <button onClick={onFileUpload}>
-        Upload!
-      </button>
-
+      <canvas ref={canvasRef}></canvas>
       <div className="slides">{slides}</div>
-
     </div>
   );
+
+
 }
 
 export default App;
--- src/slide.js
+++ src/slide.js
@@ -5,23 +5,16 @@ function Slide(props) {
     <div>
       <div className="slide">
 
-
-        <video src={props.vid} width="500" controls/>
+        <p className="timeStamp">{props.time}</p>
+        <video src={props.vid} width="32%" controls/>
 
         <p className="text">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Sit amet purus gravida quis blandit. Velit aliquet sagittis id consectetur purus ut faucibus pulvinar elementum. Suspendisse faucibus interdum posuere lorem ipsum dolor. Tellus elementum sagittis vitae et leo. Orci ac auctor augue mauris augue neque gravida in. Blandit cursus risus at ultrices mi. Ac tortor vitae purus faucibus ornare suspendisse. Mauris sit amet massa vitae tortor condimentum. Dolor morbi non arcu risus quis. Mi in nulla posuere sollicitudin aliquam ultrices sagittis orci a. Risus sed vulputate odio ut enim blandit volutpat maecenas. Bibendum ut tristique et egestas quis ipsum suspendisse. Eget gravida cum sociis natoque penatibus et magnis. Feugiat vivamus at augue eget arcu. Etiam dignissim diam quis enim. Nunc sed blandit libero volutpat sed cras ornare arcu dui. Pharetra pharetra massa massa ultricies mi. Sit </p>
-
       </div>
-
-      <div className="divider"/>
     </div>
-
-
-
-
   )
 }
 
-//<img width="250" src={props.img} />
+//<img width="250" src={props.img} />   <div className="divider"/>
 //<div>{props.time}</div>
 
 export default Slide;
